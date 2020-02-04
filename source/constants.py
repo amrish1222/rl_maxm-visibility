8d65ed4653292be6e027bcbbcbb5dbb3995a6c0e
amrish1222 <amrish1222@gmail.com>
2020-02-04 02:40:32 -0500

    updated required constants for agent

---
+++ source/constants.py
@@ -0,0 +1,28 @@
+	# -*- coding: utf-8 -*-
+# author Amrish Bakaran
+# author Adheesh
+# author Bala Murali
+# Copyright
+# Constants
+
+RENDER_PYGAME = True
+
+
+
+## Drone Constants
+class CONST:
+    def __init__(self):
+        self.TIME_STEP=1
+        
+        
+        self.NUM_DRONES = 1
+        self.MAX_AGENT_VEL= 1   
+        
+        self.GRID_SZ = self.MAX_AGENT_VEL * self.TIME_STEP * 1.0
+
+        self.MAX_STEPS = 50
+
+## Area
+        
+
+
