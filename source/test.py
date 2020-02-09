33a963c806dbf2a0ff5a8a660cdab73d0de80989
amrish1222 <amrish1222@gmail.com>
2020-02-08 19:39:39 -0500

    added keypress using cv to move agent manually

--- source/test.py
+++ source/test.py
@@ -1,22 +1,42 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  6 19:23:19 2020
 
-@author: amris
-"""
-
+import numpy as np
 from env import Env
 from tqdm import tqdm
 import cv2
+import random as rand
+
+np.set_printoptions(threshold = np.inf, linewidth = 1000 ,precision=3, suppress=True)
+
 env = Env()
 
 NUM_EPISODES = 100
 LEN_EPISODES = 200
 
+act = 0 
+wait = True
+
 for episode in tqdm(range(NUM_EPISODES)):
     for step in range(LEN_EPISODES):
         # step agent
-        actions = [1]
+        actions = [act]
         agentPosList, rewardList = env.step(actions)
-        print(agentPosList)
-        cv2.waitKey(0)
+#        print(agentPosList)
+        env.render()
+        wait = True
+        while(wait):
+            k = cv2.waitKeyEx(1) 
+#            print(k)
+            if k == 2490368:
+                act = 1
+                wait = False
+            elif k == 2424832:
+                act = 2
+                wait = False
+            elif k == 2621440:
+                act = 3
+                wait = False
+            elif k == 2555904:
+                act = 4
+                wait = False
+            
